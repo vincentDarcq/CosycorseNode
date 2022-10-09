0c9a0658b82c40100a536daa82208606604df596
Vincent Darcq <vincentdarcq@MacBook-Air-de-Vincent.local>
2022-10-09 22:16:38 +0200

    validation on popup forms & feature reset pwd

--- server/controllers/emails.js
+++ server/controllers/emails.js
@@ -62,3 +62,22 @@ exports.sendMailForBooking = async (req, res, next) => {
     res.status(200).json("Demande envoyée avec succès");
   });
 }
+
+exports.forgotPassword = async (req, res, next) => {
+  let mailOptions = {
+    from: '"Cosycorse" <vincent.darcqf78@gmail.com>', // sender address
+    to: req.query.mail, // list of receivers
+    subject: "Mot de passe oublié", // Subject line
+    text: req.body.message, // plain text body
+    html: 
+    `<b>Vous pouvez changer votre mot de passe en cliquant sur le bouton suivant :</b>
+    <button style="background-color: green; border: solid 1px green; margin: auto; color: black;" href="#">Changer mon mot de passe</button>`
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).json(error);;
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    res.status(200).json("Un email a été envoyé à votre adresse");
+  });
+}
--- server/routes/mails.js
+++ server/routes/mails.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { 
-    contactHost
+    contactHost,
+    forgotPassword
  } = require('../controllers/emails');
 
 
-//router.post('/forgotPass', forgotPassword);
+router.get('/forgotPass', forgotPassword);
 router.post('/contactHost', contactHost);
 
 module.exports = router;
