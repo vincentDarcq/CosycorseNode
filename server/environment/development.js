5f9032333eefb46cb628e6cb9da96dfc3ba2006b
Vincent Darcq <vincentdarcq@MacBook-Air-de-Vincent.local>
2022-10-15 23:53:08 +0200

    extern tokens

--- server/environment/development.js
+++ server/environment/development.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const { mongoPass } = require('../tokens/development')
 
 module.exports = {
-  dbUrl: "mongodb+srv://vincent:uTLhewpTC1@cluster0.ic0uz.mongodb.net/EventMap?retryWrites=true&w=majority",
+  dbUrl: `mongodb+srv://vincent:${mongoPass}@cluster0.ic0uz.mongodb.net/EventMap?retryWrites=true&w=majority`,
   cert: path.join(__dirname, '../ssl/cert.pem'),
   key: path.join(__dirname, '../ssl/key.pem'),
   portHttp: 300,
